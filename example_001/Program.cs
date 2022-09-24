de367fd01baa84972c9b41f876f3ebb9d3fe827e
diadiawowka <v.kucinsky@gmail.com>
2022-09-24 11:54:26 +0300

    adc

--- example_001/Program.cs
+++ example_001/Program.cs
@@ -6,5 +6,18 @@ int b2 = 23;
 int c2 = 33;
 int a3 = 13;
 int b3 = 23;
-int c3 = 33;
+int c3 = 3313;
 
+int max = a1;
+if(b1 > max) max = b1;
+if(c1 > max) max = c1;
+
+if(a2 > max) max = a2;
+if(b2 > max) max = b2;
+if(c2 > max) max = c2;
+
+if(a3 > max) max = a3;
+if(b3 > max) max = b3;
+if(c3 > max) max = c3;
+
+Console.WriteLine(max);
