67f1637979b64cc896524fea0bf6aec442592d87
diadiawowka <v.kucinsky@gmail.com>
2022-09-24 11:28:15 +0300

    second

--- example_001/Program.cs
+++ example_001/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+
